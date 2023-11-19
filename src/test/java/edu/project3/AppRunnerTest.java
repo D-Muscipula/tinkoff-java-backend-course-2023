856be135209cdbf9c52249ec6fd21f4b4c28ba58
Sergey <s4-7@yandex.ru>
2023-11-19 23:36:07 +0300

    fix test

--- src/test/java/edu/project3/AppRunnerTest.java
+++ src/test/java/edu/project3/AppRunnerTest.java
@@ -7,7 +7,7 @@ public class AppRunnerTest {
     @Test
     void test() {
         Assertions.assertDoesNotThrow(() -> {
-            String[] args = {"--path", "src\\test\\java\\edu\\project3\\logs.txt", "--from 2005-08-31", "--format markdown"};
+            String[] args = {"--path", "src/test/java/edu/project3/TestFiles/someLogs.txt", "--from 2005-08-31", "--format markdown"};
             String stringArguments = String.join(" ", args);
             Arguments arguments = Arguments.getCommandFromString(stringArguments);
             AppRunner.run(arguments);
