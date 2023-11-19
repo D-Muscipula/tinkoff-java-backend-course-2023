1476a25597f0b1d5ded674f760a0c0f623f660ca
Sergey <s4-7@yandex.ru>
2023-11-19 22:22:49 +0300

    fix test

--- src/test/java/edu/project3/SourceTests/GlobSourceTest.java
+++ src/test/java/edu/project3/SourceTests/GlobSourceTest.java
@@ -9,17 +9,9 @@ public class GlobSourceTest {
     void globSourceTest() {
         GlobSource globSource = new GlobSource();
         String expected = """
-            file1
-            file 1
             file1_1
             file 1_1""";
-        String result = globSource.getData("**test/**file1*.txt");
-        Assertions.assertEquals(expected, result);
-
-        expected = """
-            file1_1
-            file 1_1""";
-        result = globSource.getData("**test/**file1_1.txt");
+        String result = globSource.getData("**test/**file1_1.txt");
         Assertions.assertEquals(expected, result);
     }
 }
