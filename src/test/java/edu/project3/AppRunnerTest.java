e55d6d296afd8233f180c3d22eef27387aea8a25
Sergey <s4-7@yandex.ru>
2023-11-19 23:26:06 +0300

    add test

---
+++ src/test/java/edu/project3/AppRunnerTest.java
@@ -0,0 +1,16 @@
+package edu.project3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppRunnerTest {
+    @Test
+    void test() {
+        Assertions.assertDoesNotThrow(() -> {
+            String[] args = {"--path", "src\\test\\java\\edu\\project3\\logs.txt", "--from 2005-08-31", "--format markdown"};
+            String stringArguments = String.join(" ", args);
+            Arguments arguments = Arguments.getCommandFromString(stringArguments);
+            AppRunner.run(arguments);
+        });
+    }
+}
